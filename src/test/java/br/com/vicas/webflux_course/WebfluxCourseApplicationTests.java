79d9f64e67d8acb9d31fc00ee236b2b28d9cb0fd
guilhermeVicari <guilhermevicari19@outlook.com>
2024-08-18 12:14:59 -0300

    novo repositório para meu curso de programação reativa

---
+++ src/test/java/br/com/vicas/webflux_course/WebfluxCourseApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.vicas.webflux_course;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebfluxCourseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
